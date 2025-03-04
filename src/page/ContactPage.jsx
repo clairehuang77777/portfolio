aa316d1c6f22e426a0087593ae4392088165a274
huang <clairehuang77777@gmail.com>
2025-03-04 23:39:41 +0800

    add contact page

--- src/page/ContactPage.jsx
+++ src/page/ContactPage.jsx
@@ -9,10 +9,11 @@ export const ContactPage = () => {
         <div className=""></div>
       </div>
       <div className="contact-area">
-        <div className="Hello">Hello</div>
-        <div className="Hello-text">Need a beautiful, well-structured website that you can own and maintain? Get in touch!</div>
+        <div className="Hello">Hello.</div>
+        <div className="Hello-text">Feel free to contact me!</div>
         <div className="Hello-contact">
           <p>Email: clairehuang77777@gmail.com</p>
+          
         </div>
       </div>
     </div>
