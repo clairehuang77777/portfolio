31c3610cfc6af4c84c5df1d44e954b90e6878c02
huang <clairehuang77777@gmail.com>
2025-03-05 10:37:56 +0800

    fix project page display

--- src/page/ProjectPage.jsx
+++ src/page/ProjectPage.jsx
@@ -19,20 +19,20 @@ export const ProjectPage = () => {
         <div className="project1">
           <div className="project1-photo">
             <img className="project1-photo-img" src="/p1.png"></img>
-            <a href="https://cancer-screening-helper-1.onrender.com/" className={clsx("go-btn-1",{"animate__pulse":mouseEnterGO1})} onMouseEnter={()=>setMouseEnterGO1(true)} onMouseLeave={()=>setMouseEnterGO1(false)}>GO</a>
+            <a href="https://cancer-screening-helper-1.onrender.com/" className={clsx("go-btn-1",{"animate__animated animate__pulse":mouseEnterGO1})} onMouseEnter={()=>setMouseEnterGO1(true)} onMouseLeave={()=>setMouseEnterGO1(false)}>GO</a>
             </div>
           <div className="project1-description">
-             <div className="project1-title"> HEALTH-CHECK MAP</div>
-             <div className="project1-detail">Fill in your age and gender, it will show the qualified health check area that you can do. and also show the nearest clinic to do the health check by getting your position. </div>
+             <div className="project1-title"> HEALTH CHECK MAP</div>
+             <div className="project1-detail">Show the qualified health check area that you can do by filling your gender and age, also show the nearest clinic to do the health check by getting your position. </div>
           </div>
         </div>
         <div className="project2">
           <div className="project2-photo">
             <img className="project2-photo-img" src="/p2.png"></img>
-            <a href="https://clairehuang77777.github.io/Alpha-Tweet/" className={clsx("go-btn-2",{"animate__pulse":mouseEnterGO2})} onMouseEnter={()=>setMouseEnterGO2(true)} onMouseLeave={()=>setMouseEnterGO2(false)}>GO</a>
+            <a href="https://clairehuang77777.github.io/Alpha-Tweet/" className={clsx("go-btn-2",{"animate__animated animate__pulse":mouseEnterGO2})} onMouseEnter={()=>setMouseEnterGO2(true)} onMouseLeave={()=>setMouseEnterGO2(false)}>GO</a>
           </div>
           <div className="project2-description"><div className="project2-title"> ALPHA TWITTER</div>
-             <div className="project2-detail">It's a social media website, where you can post, press like and reply to other. It's support RWD & darkmode. You can use this test account: claire/abc123 to login. </div></div>
+             <div className="project2-detail">It's a twitter-like social media website, where you can post, press like and reply to other. Support RWD and darkmode. Use this test account: claire/abc123 to login. </div></div>
         </div>
       </div>
       </div>
